59b0ba24adbb98c09ddeee548ade423681803069
Jim Cronqvist <jim.cronqvist@gmail.com>
2022-05-13 14:58:35 +0200

    Initial commit

--- copy.sh
+++ copy.sh
@@ -1,21 +1,25 @@
 #!/bin/env bash
 
+PACKAGE="vue-common"
+SCOPE="@cronqvist"
+
+
 DIR="$(pwd)"
-MOUNT_CHECK="packages/vue-common"
+MOUNT_CHECK="packages/$PACKAGE"
 
 # If the script is ran from the context of being a node_modules dependency
 if [ "$(pwd | grep -c 'node_modules')" -gt 0 ]; then
     PACKAGES_DIR="../../../packages"
-    PACKAGE_DIR="../../../packages/vue-common"
+    PACKAGE_DIR="../../../packages/$PACKAGE"
     NODE_MODULES_PACKAGE_DIR="$DIR"
 else
     PACKAGES_DIR="packages"
-    PACKAGE_DIR="packages/vue-common"
-    NODE_MODULES_PACKAGE_DIR="node_modules/@cronqvist/vue-common"
+    PACKAGE_DIR="packages/$PACKAGE"
+    NODE_MODULES_PACKAGE_DIR="node_modules/$SCOPE/$PACKAGE"
 fi
 
 # Symlink it does not work on Windows as it can't understand symlinks across Windows
-#mkdir -p "packages/" && ln -rTs "node_modules/@cronqvist/vue-common" "packages/vue-common"
+#mkdir -p "packages/" && ln -rTs "node_modules/$SCOPE/$PACKAGE" "packages/vue-common"
 
 # If there is no mount, copy the files
 if [ "$(mount | grep -c "/$MOUNT_CHECK")" -eq 0 ]; then
