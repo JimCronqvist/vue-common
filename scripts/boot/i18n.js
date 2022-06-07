2b05b9d5d0dfc94da78ca29f124a0ed06440798f
Jim Cronqvist <jim.cronqvist@gmail.com>
2022-06-07 22:14:58 +0200

    Initial commit

--- scripts/boot/i18n.js
+++ scripts/boot/i18n.js
@@ -1,18 +1,8 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n'
 import axios from 'axios';
-//import en from "vee-validate/dist/locale/en.json";
-//import sv from "vee-validate/dist/locale/sv.json";
-import { merge } from 'lodash';
 
 Vue.use(VueI18n);
-
-export function boot({ app }) {
-  // Load the default language
-  setI18nLanguage(selectedLocale);
-
-  return {i18n};
-}
 
 function loadLocaleMessages() {
   //console.log('start load locale message');
@@ -29,65 +19,57 @@ function loadLocaleMessages() {
   return messages;
 }
 
-function checkDefaultLanguage () {
+function checkDefaultLanguage(storeLocale) {
   let matched = null;
   const supportedLanguages = Object.getOwnPropertyNames(loadLocaleMessages());
-  matched = supportedLanguages.find(lang => lang === navigator.language);
-  if (!matched) {
+
+  matched = supportedLanguages.find(lang => lang === storeLocale);
+  if(!matched) {
+    matched = supportedLanguages.find(lang => lang === navigator.language);
+  }
+  if(!matched) {
     const navigatorLanguagePartials = navigator.language.split('-')[0];
     matched = supportedLanguages.find(lang => lang === navigatorLanguagePartials);
   }
-  if (!matched) {
+  if(!matched) {
     const navigatorLanguagePartials = navigator.language.split('-')[0];
     matched = supportedLanguages.find(lang => lang.split('-')[0] === navigatorLanguagePartials);
   }
   return matched;
 }
 
-export const languages = Object.getOwnPropertyNames(loadLocaleMessages());
-export const selectedLocale = checkDefaultLanguage() || import.meta.env.VITE_I18N_LOCALE || 'en-US';
-export const selectedLanguage = selectedLocale.split('-')[0];
-
-// Temporary print out i18n info.
-/*
-console.log(selectedLocale, languages, merge(loadLocaleMessages(), {
-  'sv-SE': {
-    validations: sv.messages
-  },
-  'en-US': {
-    validations: en.messages
-  }
-}));
-*/
-
 // If we want to add different dateTimeFormats per locale in the future:
 // See: https://github.com/preetishhs/vue-localization-techniques/blob/master/src/locales/formats/dataTimeFormats.js
 
-export function setI18nLanguage(lang) {
+export async function setI18nLanguage(lang) {
   console.log('Set i18n language to '+lang);
   if(!languages.includes(lang)) return Promise.reject('setI18nLanguage: i18n language does not exist');
 
   return loadLocaleMessages()[lang]().then(messages => {
-    i18n.setLocaleMessage(lang, messages.default);
+    i18n.mergeLocaleMessage(lang, messages.default);
     i18n.locale = lang;
     axios.defaults.headers.common['Accept-Language'] = lang;
     document.querySelector('html').setAttribute('lang', lang);
   });
 }
 
+export const languages = Object.getOwnPropertyNames(loadLocaleMessages());
+export const selectedLocale = storeLocale => checkDefaultLanguage(storeLocale) || import.meta.env.VITE_I18N_LOCALE || 'en-US';
+export const selectedLanguage = selectedLocale => selectedLocale.split('-')[0];
+
 const i18n = new VueI18n({
-  locale: selectedLocale,
   fallbackLocale: import.meta.env.VITE_I18N_LOCALE || 'en-US',
-  /*
-  messages: merge(loadLocaleMessages(), {
-    'sv-SE': {
-      validations: sv.messages
-    },
-    'en-US': {
-      validations: en.messages
-    }
-  }),
-  */
 });
+
+export async function boot({ app, store }) {
+
+  const locale = selectedLocale(store.state.locale);
+  //const language = selectedLanguage(locale);
+
+  // Load the default language
+  await setI18nLanguage(locale);
+
+  return {i18n};
+}
 
 export default i18n;
