2f75c366d56b27323851ca5a00ee027ccaf0a750
Jim Cronqvist <jim.cronqvist@gmail.com>
2024-06-10 10:22:04 +0200

    Fix incorrect reference

--- components/vuetify2/VDataTableServer.vue
+++ components/vuetify2/VDataTableServer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="v-data-table-server">
-    <v-data-table v-bind="getVDataTableAttributes" v-on="this.$listeners" :page="page">
+    <v-data-table v-bind="getVDataTableAttributes" v-on="$listeners" :page="page">
       <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
       <template v-for="(_, name) in $scopedSlots" :slot="name" slot-scope="slotData"><slot :name="name" v-bind="slotData" /></template>
     </v-data-table>
