0b969beb5531bfcf58f2bc9b97d47cbc855d45f3
Jim Cronqvist <jim.cronqvist@gmail.com>
2026-02-11 17:04:15 +0100

    Refactored the i18n implementation to be more generic

--- scripts/boot/i18n.js
+++ scripts/boot/i18n.js
@@ -3,6 +3,14 @@ import { useLocaleStore } from '../../stores/locale';
 import { markRaw } from 'vue';
 import { changeI18nLocale } from '../../packages/i18n';
 
+
+// Create a global i18n to be able to export the global properties
+let i18nSingletonUsed = false;
+const i18nSingleton = createI18n({
+  legacy: false, // Vuetify does not support the legacy mode of vue-i18n
+  fallbackLocale: import.meta.env.VITE_I18N_LOCALE || 'en-US',
+});
+
 // If we want to add different dateTimeFormats per locale in the future:
 // See: https://github.com/preetishhs/vue-localization-techniques/blob/master/src/locales/formats/dataTimeFormats.js
 
@@ -15,10 +23,18 @@ export function createI18nPiniaPlugin(i18n) {
 }
 
 export async function boot(app, { $pinia, $http }) {
-  const i18n = createI18n({
-    legacy: false, // Vuetify does not support the legacy mode of vue-i18n
-    fallbackLocale: import.meta.env.VITE_I18N_LOCALE || 'en-US',
-  });
+  // The first time we boot, we will use the global i18n instance. The other times, create a local instance.
+  let i18n;
+  if(!i18nSingletonUsed) {
+    i18n = i18nSingleton;
+    i18nSingletonUsed = true;
+  }
+  if(!i18n) {
+    i18n = createI18n({
+      legacy: false, // Vuetify does not support the legacy mode of vue-i18n
+      fallbackLocale: import.meta.env.VITE_I18N_LOCALE || 'en-US',
+    });
+  }
 
   app.use(i18n);
   $pinia.use(createI18nPiniaPlugin(i18n));
@@ -28,4 +44,8 @@ export async function boot(app, { $pinia, $http }) {
 
   const localeStore = useLocaleStore($pinia);
   await localeStore.registerChangeLocaleCallback(true, async (locale) => await changeI18nLocale(i18n, locale, $http));
+  console.log('boot i18n');
 }
+
+export const { t, te, tc, d, n } = i18nSingleton.global;
+export default i18nSingleton;
